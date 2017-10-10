cceb39f11cf444da356068062ba4349e631ffb07
NanCarp <nancarpli@163.com>
2017-10-10 11:16:12 +0800

    迭代器与组合模式

---
+++ src/com/github/nancarp/ch9/iterator/NullIterator.java
@@ -0,0 +1,20 @@
+package com.github.nancarp.ch9.iterator;
+
+import java.util.Iterator;
+
+public class NullIterator implements Iterator {
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
